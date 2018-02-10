8d5fa47f30160c20ea15838fec1d0c5f7d96033f
Alireza Ahmadi <alireza.elecat@gmail.com>
2018-02-10 10:35:31 +0100

    Final
    input re-arrange

--- X2Lidar/X2Lidar.ino
+++ X2Lidar/X2Lidar.ino
@@ -20,7 +20,7 @@ OctoSonarX2 myOcto(SONAR_ADDR, SONAR_INT);
 ros::NodeHandle  nh;
 sensor_msgs::LaserScan scan;
 ros::Publisher pub_range( "scan", &scan);
-float ranges[16];
+float ranges[12];
 char frameid[] = "/base_link";
 
      
@@ -31,8 +31,16 @@ void setup()
   nh.initNode();
   delay(1000);
   nh.advertise(pub_range);
+  scan.header.frame_id = "base_footprint";
   scan.header.stamp=nh.now();
-  scan.ranges_length=16;
+  scan.angle_min=0;
+  scan.angle_max=0.6;
+  scan.time_increment=0;
+  scan.angle_increment=(0.26179938779)*2;
+  scan.range_min=0.03;
+  scan.range_max=5.0;
+  scan.ranges_length=12;
+  scan.intensities_length=12;
   scan.ranges= ranges;
 } 
 
@@ -45,27 +53,34 @@ int32_t i=0;
 
 void loop()
 {
-  if (last_print + 999 < millis()) {   // serial output every 200ms
-    last_print = millis();
-    i=count;
-    count =0;
-  }
+//  if (last_print + 999 < millis()) {   // serial output every 200ms
+//    last_print = millis();
+//    i=count;
+//    count =0;
+//  }
+//  scan.ranges[15]=i; 
+//  count++;
   
   OctoSonar::doSonar();  // call every cycle, OctoSonar handles the spacing  
   
   for (uint8_t i = 0; i < 16; i++) {
     Distance[i] = myOcto.read(i);
   }
-  
-  for(int j=0;j<16;j++){
-    scan.ranges[j] =Distance[j];
-    
-  }
-  
-  scan.ranges[i]=i; 
-  count++;
-  
+
+  scan.ranges[0]=(float)Distance[1]/100;
+  scan.ranges[1]=(float)Distance[2]/100;
+  scan.ranges[2]=(float)Distance[0]/100;
+  scan.ranges[3]=(float)Distance[5]/100;
+  scan.ranges[4]=(float)Distance[6]/100;
+  scan.ranges[5]=(float)Distance[4]/100;
+  scan.ranges[6]=(float)Distance[9]/100;
+  scan.ranges[7]=(float)Distance[10]/100;
+  scan.ranges[8]=(float)Distance[8]/100;
+  scan.ranges[9]=(float)Distance[13]/100;
+  scan.ranges[10]=(float)Distance[14]/100;
+  scan.ranges[11]=(float)Distance[12]/100;
+
   pub_range.publish(&scan);
   nh.spinOnce();
-  delay(50);
+  delay(35);
 }
