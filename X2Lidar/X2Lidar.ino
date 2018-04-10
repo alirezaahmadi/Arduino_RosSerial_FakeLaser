0450ec8e062db1514a227dfedc6c5fae976870b3
Alireza <alireza.elecat@gmail.com>
2018-04-10 08:33:38 +0200

    Circular Patern
    just added extra fake points based on measurements !! to make circular patern

--- X2Lidar/X2Lidar.ino
+++ X2Lidar/X2Lidar.ino
@@ -20,7 +20,7 @@ OctoSonarX2 myOcto(SONAR_ADDR, SONAR_INT);
 ros::NodeHandle  nh;
 sensor_msgs::LaserScan scan;
 ros::Publisher pub_range( "scan", &scan);
-float ranges[12];
+float ranges[60];
 char frameid[] = "/base_link";
 
      
@@ -36,11 +36,11 @@ void setup()
   scan.angle_min=0;
   scan.angle_max=0.6;
   scan.time_increment=0;
-  scan.angle_increment=(0.26179938779)*2;
+  scan.angle_increment=0.052359877559833*2;  //(0.26179938779)*2;
   scan.range_min=0.03;
   scan.range_max=5.0;
-  scan.ranges_length=12;
-  scan.intensities_length=12;
+  scan.ranges_length=60;
+  scan.intensities_length=10;
   scan.ranges= ranges;
 } 
 
@@ -68,18 +68,42 @@ void loop()
   }
 
   scan.ranges[0]=(float)Distance[1]/100;
-  scan.ranges[1]=(float)Distance[2]/100;
-  scan.ranges[2]=(float)Distance[0]/100;
-  scan.ranges[3]=(float)Distance[5]/100;
-  scan.ranges[4]=(float)Distance[6]/100;
-  scan.ranges[5]=(float)Distance[4]/100;
-  scan.ranges[6]=(float)Distance[9]/100;
-  scan.ranges[7]=(float)Distance[10]/100;
-  scan.ranges[8]=(float)Distance[8]/100;
-  scan.ranges[9]=(float)Distance[13]/100;
-  scan.ranges[10]=(float)Distance[14]/100;
-  scan.ranges[11]=(float)Distance[12]/100;
+  for(uint8_t i = 0; i <= 10; i++)  scan.ranges[i]=scan.ranges[0];
+  
+  scan.ranges[6]=(float)Distance[2]/100;
+  for(uint8_t i = 6; i <= 10; i++)  scan.ranges[i]=scan.ranges[6];
+  
+  scan.ranges[11]=(float)Distance[0]/100;
+  for(uint8_t i = 11; i <= 15; i++)  scan.ranges[i]=scan.ranges[11];
+  
+  scan.ranges[16]=(float)Distance[5]/100;
+  for(uint8_t i = 16; i <= 20; i++)  scan.ranges[i]=scan.ranges[16];
+  
+  scan.ranges[21]=(float)Distance[6]/100;
+  for(uint8_t i = 21; i <= 25; i++)  scan.ranges[i]=scan.ranges[21];
+  
+  scan.ranges[26]=(float)Distance[4]/100;
+  for(uint8_t i = 26; i <= 30; i++)  scan.ranges[i]=scan.ranges[26];
+  
+  scan.ranges[31]=(float)Distance[9]/100;
+  for(uint8_t i = 31; i <= 35; i++)  scan.ranges[i]=scan.ranges[31];
+  
+  scan.ranges[36]=(float)Distance[10]/100;
+  for(uint8_t i = 36; i <= 40; i++)  scan.ranges[i]=scan.ranges[36];
+  
+  scan.ranges[41]=(float)Distance[8]/100;
+  for(uint8_t i = 41; i <= 45; i++)  scan.ranges[i]=scan.ranges[41];
+  
+  scan.ranges[46]=(float)Distance[13]/100;
+  for(uint8_t i = 46; i <= 50; i++) scan.ranges[i]=scan.ranges[46];
+  
+  scan.ranges[51]=(float)Distance[14]/100;
+  for(uint8_t i = 51; i <= 55; i++)  scan.ranges[i]=scan.ranges[51];
+  
+  scan.ranges[56]=(float)Distance[12]/100;
+  for(uint8_t i = 56; i < 60; i++)  scan.ranges[i]=scan.ranges[565];
 
+  
   pub_range.publish(&scan);
   nh.spinOnce();
   delay(35);
